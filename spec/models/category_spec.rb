fa1aa763ec78d15b2eda50ecc052a1a000ddd2b1
Antonio Pacheco <greven77@gmail.com>
2016-05-06 15:51:21 +0000

    Added tests for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:category) { FactoryGirl.create(:category) }
+  let!(:subcategory_a) { FactoryGirl.create(:category, parent_id: category.id) }
+  let!(:subcategory_b) { FactoryGirl.create(:category, parent_id: category.id) }
+  before do
+    4.times { subcategory_a.products.create(FactoryGirl.attributes_for(:product)) }
+    6.times { subcategory_b.products.create(FactoryGirl.attributes_for(:product) )}
+  end
+
+  it { should have_many :products }
+  it { should have_many :children }
+  it { should belong_to :parent }
+
+  describe "category" do
+    it "should have children" do
+      expect(category.has_children?).to be true
+    end
+
+    it "should have two subcategories" do
+      expect(category.children.count).to eq(2)
+    end
+  end
+
+  describe "subcategories" do
+    it "should add the correct amount of products to subcategories" do
+      expect(subcategory_a.products.count).to eq(4)
+      expect(subcategory_b.products.count).to eq(6)
+    end
+
+    it "should have a parent" do
+      expect(subcategory_a.has_parent?).to be true
+    end
+
+    it "should have a parent name" do
+      expect(subcategory_a.parent_name).not_to be_empty
+    end
+  end
 end
